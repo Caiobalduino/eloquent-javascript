87f5f04b6534424efb88f3951c88d81d909e176b
Caio Balduino <caiobrafachino@hotmail.com>
2025-02-26 14:31:22 -0300

    fizzbuzz exercise chapter2

--- chapter_2/exercises/ex_1.js
+++ chapter_2/exercises/ex_1.js
@@ -23,4 +23,24 @@ for (let i = 1; i <= 7; i++) {
     line += "#";
   }
   console.log(line);
+} 
+
+/* 
+Write a program that uses console.log to print all the numbers from 1 to 100, with 
+two exceptions. For number divisible by 3, print "Fizz" instead of the number, and for numbers divisible
+by 5 (and not 3), print "Buzz" instead.
+And print "FizzBuzz" for numbers that are divisible by both 3 and 5. 
+(and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+*/
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  } 
 }
